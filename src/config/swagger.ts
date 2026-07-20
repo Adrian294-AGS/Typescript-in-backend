40d9573e213d1339ca613c07d832e7a599380ca3
Adrian <stgoadrian294@gmail.com>
2026-07-21 00:01:48 +0800

    swagger commented

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -1,28 +1,28 @@
-import swaggerJsDoc from "swagger-jsdoc";
-import type {Options} from "swagger-jsdoc";
+// import swaggerJsDoc from "swagger-jsdoc";
+// import type {Options} from "swagger-jsdoc";
 
-const option: Options = {
-    definition: {
-        openapi: "3.0.0",
-        info: {
-            title: "For Typescript course",
-            version: "1.0.0",
-            description: "A minimal testing app for typescript course"
-        },
-        servers: [{ url: "http://localhost:5000"}],
-        components: {
-            securitySchemes: {
-                bearerAuth: {
-                    type: "http",
-                    scheme: "bearer",
-                    bearerFormat: "JWT"
-                }
-            }
-        },
-        security: [{bearerAuth: []}],
-    },
-    apis: ["./src/routes/*.ts"],
-};
+// const option: Options = {
+//     definition: {
+//         openapi: "3.0.0",
+//         info: {
+//             title: "For Typescript course",
+//             version: "1.0.0",
+//             description: "A minimal testing app for typescript course"
+//         },
+//         servers: [{ url: "http://localhost:5000"}],
+//         components: {
+//             securitySchemes: {
+//                 bearerAuth: {
+//                     type: "http",
+//                     scheme: "bearer",
+//                     bearerFormat: "JWT"
+//                 }
+//             }
+//         },
+//         security: [{bearerAuth: []}],
+//     },
+//     apis: ["./src/routes/*.ts"],
+// };
 
-const swaggerSpec = swaggerJsDoc(option);
-export default swaggerSpec;
+// const swaggerSpec = swaggerJsDoc(option);
+// export default swaggerSpec;
